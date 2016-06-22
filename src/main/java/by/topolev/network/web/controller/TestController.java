413f9a88154787bdbd61d50eb8b6c9564cd3816d
Topolev <i.topolev.vladimir@gmail.com>
2016-06-22 14:40:22 +0300

    add http client

--- src/main/java/by/topolev/network/web/controller/TestController.java
+++ src/main/java/by/topolev/network/web/controller/TestController.java
@@ -1,13 +1,26 @@
 package by.topolev.network.web.controller;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jcabi.http.Request;
+import com.jcabi.http.request.ApacheRequest;
+import com.jcabi.http.request.JdkRequest;
+import com.jcabi.http.response.RestResponse;
+
 @Controller
 public class TestController {
-	
-	@RequestMapping(value="/test", method=RequestMethod.GET)
-	public void testMethod(){
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String testMethod() throws IOException {
 		System.out.println("TEST");
+		String html = new JdkRequest("https://www.google.com").fetch().body();
+				  
+		System.out.println(html);
+		return "test";
 	}
 }
